1901664382c0f7aec93e85c7a1375620ec0941dc
Oksana <73747282+latkova@users.noreply.github.com>
2020-11-07 19:37:50 +0200

    Update FirstClass.java

--- FirstClass.java
+++ FirstClass.java
@@ -12,29 +12,29 @@ package com.company;
 
                 long exponent = (long) Math.pow(10,
                         9);
-                long total_quant = 9 * exponent;
+                long totalQuant = 9 * exponent;
 
-                System.out.println("Total quantity of 10-digit numbers = 9*10 to the power of 9 = " + total_quant);
+                System.out.println("Total quantity of 10-digit numbers = 9*10 to the power of 9 = " + totalQuant);
 
                 /* Compute the quantity of 10-digit numbers, which have different digits
                        factorial 9! to be computed
                 */
 
-                int diff_quant = 1;
+                int diffQuant = 1;
                 int i = 1;
                 while (i <= 9) {
-                    diff_quant *= i;
+                    diffQuant *= i;
                     i++;
                 }
-                System.out.println("The quantity of 10-digit numbers, which have different digits = 9*9! = " + 9 * diff_quant);
+                System.out.println("The quantity of 10-digit numbers, which have different digits = 9*9! = " + 9 * diffQuant);
 
             /* compute the quantity of 10-digit numbers, which have at least 2 identical digits
                 to solve the task - from the total quantity of 10-digit numbers we should deduct
                 the quantity of 10-digit numbers, which have different digits
              */
                 
-                long identic_quant = total_quant - 9 * diff_quant;
-                System.out.println("The quantity of 10-digit numbers, which have at least 2 identical digits = " + identic_quant);
+                long identicQuant = totalQuant - 9 * diffQuant;
+                System.out.println("The quantity of 10-digit numbers, which have at least 2 identical digits = " + identicQuant);
 
             }
 }
